1f691ad1e942ca15fb07cdee57250a5373d18fe0
Mihail Marian <mihailthebuilder@gmail.com>
2021-05-21 01:39:20 +0100

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import {useState} from "react";
+import { useState } from "react";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
-  
+  const [romanNumeral, setRomanNumeral] = useState("");
+  const [serverResults, setServerResults] = useState({});
+
+  const getServerResults = () => {};
+
   return (
     <main>
       <h1>Input the Roman numeral</h1>
-
+      <input
+        type="text"
+        value={romanNumeral}
+        onChange={(event) => setRomanNumeral(event.target.value)}
+      />
+      <button onCLick={getServerResults}>Calculate</button>
     </main>
   );
 }
