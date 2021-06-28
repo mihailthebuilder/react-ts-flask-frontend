2a22fc763f529081192832e202962396465ed15d
Mihail Marian <mihailmarian12@gmail.com>
2021-06-28 17:50:38 +0100

    installed eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 
-import "./App.scss";
+import './App.scss';
 
 function App() {
-  const [romanNumeral, setRomanNumeral] = useState("");
+  const [romanNumeral, setRomanNumeral] = useState('');
   const [calculating, setCalculating] = useState(false);
   const [serverResults, setServerResults] = useState({});
 
   const getServerResults = () => {
     setCalculating(true);
-    const requestUrl =
-      window.location.hostname === "localhost"
-        ? "http://localhost:5000/"
-        : "https://desolate-harbor-87386.herokuapp.com/";
+    const requestUrl = window.location.hostname === 'localhost'
+      ? 'http://localhost:5000/'
+      : 'https://desolate-harbor-87386.herokuapp.com/';
 
     axios.get(requestUrl + romanNumeral).then((res) => {
       setCalculating(false);
@@ -32,11 +31,14 @@ function App() {
       <button onClick={getServerResults}>Calculate</button>
       <div className="output">
         {calculating
-          ? "Calculating..."
-          : Object.keys(serverResults).length > 0 &&
-            Object.entries(serverResults).map(([key, value]) => (
+          ? 'Calculating...'
+          : Object.keys(serverResults).length > 0
+            && Object.entries(serverResults).map(([key, value]) => (
               <p key={key}>
-                {key}: {value}
+                {key}
+                :
+                {' '}
+                {value}
               </p>
             ))}
       </div>
